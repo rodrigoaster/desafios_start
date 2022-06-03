a3d2468f613dd0825d675a585972d5652b0629bc
Rodrigo Astere <rodrigowebx@gmail.com>
2022-06-02 23:49:46 -0300

    código do desafio realizado usando for e if

---
+++ script.js
@@ -0,0 +1,16 @@
+// DESAFIO 01
+
+const escreveResultado = () => {
+    for (let i = 1; i < 299; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log(`Múltiplos de 3 e 5: ${i}, VAMBORA`)
+        } else if (i % 5 === 0) {
+            console.log(`Múltiplos de 5: ${i}, BORA`)
+        } else if  (i % 3 === 0) {
+            console.log(`Múltiplos de 3: ${i}, VAMO`)
+        }
+    }
+}
+
+escreveResultado();
+
